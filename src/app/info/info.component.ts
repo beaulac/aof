e0aa55c541087118f1bd72a8c1bbf0d458ee7e6b
Antoine Beauvais-Lacasse <abeaulac@octhuber.com>
2017-08-10 19:02:51 -0400

    Add simple info popovers.

--- src/app/info/info.component.ts
+++ src/app/info/info.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-info',
-  templateUrl: './info.component.html',
-  styleUrls: ['./info.component.css']
-})
+               selector: 'app-info',
+               templateUrl: './info.component.html',
+               styleUrls: ['./info.component.css']
+           })
 export class InfoComponent implements OnInit {
 
-  constructor() { }
+    popoverHidden = [true, true, true, true];
 
-  ngOnInit() {
-  }
+    constructor() {
+    }
 
+    ngOnInit() {
+    }
+
+    showPopover(toggleIdx) {
+        this.popoverHidden = this.popoverHidden.map((hidden, idx) => !hidden || (idx !== toggleIdx));
+    }
 }
