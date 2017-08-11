ef66c8605b4711f44ac97a93af65a8f2321944df
Antoine Beauvais-Lacasse <abeaulac@octhuber.com>
2017-08-10 22:53:15 -0400

    Make bottom bar smaller

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { CyRendererComponent } from './graph/renderer/cy-renderer.component';
 @Component({
                selector: 'app-root',
                templateUrl: './app.component.html',
-               styleUrls: ['./app.component.css']
+               styleUrls: ['./app.component.scss']
            })
 export class AppComponent implements OnInit {
 
