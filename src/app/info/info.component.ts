0c079223f1b1df0cc6558f8d255a4b57e0028517
chenglong zhang <chenglongzhang931@gmail.com>
2017-09-12 21:42:58 -0400

    Modification to fix some issues

--- src/app/info/info.component.ts
+++ src/app/info/info.component.ts
@@ -4,11 +4,11 @@ import { fadeAnimation } from '../fade.animation';
 const toggleFn = toggleIdx => (shown, idx) => (toggleIdx === idx) ? !shown : false;
 
 @Component({
-               selector: 'app-info',
-               animations: [fadeAnimation],
-               templateUrl: './info.component.html',
-               styleUrls: ['./info.component.scss']
-           })
+    selector: 'app-info',
+    animations: [fadeAnimation],
+    templateUrl: './info.component.html',
+    styleUrls: ['./info.component.scss']
+})
 export class InfoComponent implements OnInit {
     @Output()
     public rollOut = new EventEmitter<void>();
